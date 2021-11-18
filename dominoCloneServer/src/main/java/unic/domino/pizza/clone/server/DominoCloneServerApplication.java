aeed46cc3983895bc78e7128e4d6fbeb2af6bbf6
doosanpark <erid3232@naver.com>
2021-11-18 23:28:47 +0900

    시작

---
+++ dominoCloneServer/src/main/java/unic/domino/pizza/clone/server/DominoCloneServerApplication.java
@@ -0,0 +1,13 @@
+package unic.domino.pizza.clone.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominoCloneServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominoCloneServerApplication.class, args);
+	}
+
+}
---
+++ dominoCloneServer/src/test/java/unic/domino/pizza/clone/server/DominoCloneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package unic.domino.pizza.clone.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominoCloneServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
