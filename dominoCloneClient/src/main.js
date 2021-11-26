64fd227ce7945225655ad53d5ca6231462a6a06f
sykim <syk0707@naver.com>
2021-11-26 16:25:45 +0900

    [client] TestSY 작업 내용 복구

--- dominoCloneClient/src/main.js
+++ dominoCloneClient/src/main.js
@@ -3,9 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fab)
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- dominoCloneClient/src/router/index.js
+++ dominoCloneClient/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import HelloWorld from '@/components/HelloWorld'
+import TestSY from '@/components/TestSY'
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/TestSY',
+      name: 'TestSY',
+      component: TestSY
     }
   ]
 })
