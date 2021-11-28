d01cf7a4941e42ab4883b0b31d9aeeb6072314ea
doosanpark <erid3232@naver.com>
2021-11-28 19:29:01 +0900

    Revert "cli 기반 webpack으로 client  변경"
    This reverts commit 560c9ff384a4f41f0c05e8fae3212b3a550e8b4c.

--- dominoCloneClient/src/App.vue
+++ dominoCloneClient/src/App.vue
@@ -1,23 +1,15 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
-</script>
+import HelloWorld from "./components/HelloWorld.vue";
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ dominoCloneClient/src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>Home page</p>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
