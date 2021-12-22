56b48ee302f45ae1e4794183edbbbd45201494ac
doosanpark <erid3232@naver.com>
2021-12-22 10:38:53 +0900

    로그인 기능 구현

--- dominoCloneClient/src/components/LoginView.vue
+++ dominoCloneClient/src/components/LoginView.vue
@@ -12,11 +12,11 @@
 
             <!-- Using props -->
             <b-input-group>
-              <b-form-input placeholder="아이디"></b-form-input>
+              <b-form-input id="accnt" ref="accnt" placeholder="아이디"></b-form-input>
             </b-input-group>
 
             <b-input-group>
-              <b-form-input placeholder="비밀번호"></b-form-input>
+              <b-form-input id="pass" ref="pass" placeholder="비밀번호"></b-form-input>
             </b-input-group>
 
             <div class="chk_item">
@@ -32,7 +32,7 @@
               
             </div>
             <div>
-              <b-button variant="danger">로그인</b-button>
+              <b-button v-on:click="postData" variant="danger">로그인</b-button>
             </div>
             <div>
               <b-button variant="outline-primary">회원가입</b-button>
@@ -48,6 +48,9 @@
 
 <script>
 /* eslint-disable */
+import axios from "axios"
+import $ from 'jquery';
+
   export default {
     name: 'LoginView',
     data() {
@@ -58,13 +61,35 @@
       }
     },
     methods: {
-      countDownChanged(dismissCountDown) {
-        this.dismissCountDown = dismissCountDown
-      },
-      showAlert() {
-        this.dismissCountDown = this.dismissSecs
+      postData() {
+        // let paramMap = {}; 
+        // paramMap.acnnt = $("#accnt").val(); 
+        // paramMap.pass = $("#pass").val(); 
+
+        // console.log("accnt click evnt", this.$refs.accnt);
+        // console.log("pass click evnt", this.$refs.pass);
+
+        
+        // axios를 이용하여 post 방식으로 보낼 경우 FormData()형식으로 보내야 함
+        // 그렇지 않으면 spring에서 빈 데이터를 받음
+        const formData = new FormData();
+        formData.append('accnt', $("#accnt").val());
+        formData.append('pass', $("#pass").val());
+        
+        axios.post(
+              "http://localhost:8080/checkaccnt"
+              , formData
+            )
+            .then(response => {
+              this.resp = response;
+              console.log(response);
+            })
+            .catch(e => {
+              console.error(e);
+            });
       }
-    }
+    }  
+    
   }
 </script>
 <style>
