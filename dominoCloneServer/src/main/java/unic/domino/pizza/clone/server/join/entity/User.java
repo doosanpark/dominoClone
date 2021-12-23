0861f5f7affa30d991270065fa2d6a93fdc37b2c
SYKim <syk0707@naver.com>
2021-12-24 00:17:20 +0900

    [join] 접근자 코드 정리

--- dominoCloneServer/src/main/java/unic/domino/pizza/clone/server/join/entity/User.java
+++ dominoCloneServer/src/main/java/unic/domino/pizza/clone/server/join/entity/User.java
@@ -1,5 +1,8 @@
 package unic.domino.pizza.clone.server.join.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.Where;
 
 import javax.persistence.Column;
@@ -9,6 +12,9 @@ import javax.persistence.Table;
 import java.time.Instant;
 
 @Entity
+@Getter
+@Setter
+@ToString
 @Table(name = "user")
 public class User {
     @EmbeddedId
@@ -67,156 +73,4 @@ public class User {
 
     @Column(name = "cash_rcpt_info", length = 45)
     private String cashRcptInfo;
-
-    public String getCashRcptInfo() {
-        return cashRcptInfo;
-    }
-
-    public void setCashRcptInfo(String cashRcptInfo) {
-        this.cashRcptInfo = cashRcptInfo;
-    }
-
-    public Integer getCashRcpt() {
-        return cashRcpt;
-    }
-
-    public void setCashRcpt(Integer cashRcpt) {
-        this.cashRcpt = cashRcpt;
-    }
-
-    public String getCashRcptYn() {
-        return cashRcptYn;
-    }
-
-    public void setCashRcptYn(String cashRcptYn) {
-        this.cashRcptYn = cashRcptYn;
-    }
-
-    public String getRegId() {
-        return regId;
-    }
-
-    public void setRegId(String regId) {
-        this.regId = regId;
-    }
-
-    public String getModId() {
-        return modId;
-    }
-
-    public void setModId(String modId) {
-        this.modId = modId;
-    }
-
-    public Instant getLastChgdDtm() {
-        return lastChgdDtm;
-    }
-
-    public void setLastChgdDtm(Instant lastChgdDtm) {
-        this.lastChgdDtm = lastChgdDtm;
-    }
-
-    public String getExPwd() {
-        return exPwd;
-    }
-
-    public void setExPwd(String exPwd) {
-        this.exPwd = exPwd;
-    }
-
-    public String getUserLevel() {
-        return userLevel;
-    }
-
-    public void setUserLevel(String userLevel) {
-        this.userLevel = userLevel;
-    }
-
-    public String getAgreeCllctPrivtInfoYn() {
-        return agreeCllctPrivtInfoYn;
-    }
-
-    public void setAgreeCllctPrivtInfoYn(String agreeCllctPrivtInfoYn) {
-        this.agreeCllctPrivtInfoYn = agreeCllctPrivtInfoYn;
-    }
-
-    public String getBirthInfo() {
-        return birthInfo;
-    }
-
-    public void setBirthInfo(String birthInfo) {
-        this.birthInfo = birthInfo;
-    }
-
-    public Instant getAccntAvailPriod() {
-        return accntAvailPriod;
-    }
-
-    public void setAccntAvailPriod(Instant accntAvailPriod) {
-        this.accntAvailPriod = accntAvailPriod;
-    }
-
-    public String getAccntCertiYn() {
-        return accntCertiYn;
-    }
-
-    public void setAccntCertiYn(String accntCertiYn) {
-        this.accntCertiYn = accntCertiYn;
-    }
-
-    public Instant getModDtm() {
-        return modDtm;
-    }
-
-    public void setModDtm(Instant modDtm) {
-        this.modDtm = modDtm;
-    }
-
-    public Instant getRegDtm() {
-        return regDtm;
-    }
-
-    public void setRegDtm(Instant regDtm) {
-        this.regDtm = regDtm;
-    }
-
-    public Integer getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(Integer zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getUserPassword() {
-        return userPassword;
-    }
-
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
-    public UserId getId() {
-        return id;
-    }
-
-    public void setId(UserId id) {
-        this.id = id;
-    }
 }
