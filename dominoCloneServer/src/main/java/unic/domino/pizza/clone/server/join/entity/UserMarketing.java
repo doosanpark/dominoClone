3bf5e4baa06ca00a6b46c70030c76b239aab66f7
SYKim <syk0707@naver.com>
2022-01-09 23:59:01 +0900

    [server] entity 추가중

---
+++ dominoCloneServer/src/main/java/unic/domino/pizza/clone/server/join/entity/UserMarketing.java
@@ -0,0 +1,24 @@
+package unic.domino.pizza.clone.server.join.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "user_marketing")
+public class UserMarketing {
+    @Id
+    @Column(name = "user_id", nullable = false, length = 50)
+    private String userId;
+    @Column(name = "is_email")
+    private boolean isEmail;
+    @Column(name = "is_sms")
+    private boolean isSms;
+    @Column(name = "is_address")
+    private boolean isAddress;
+}
